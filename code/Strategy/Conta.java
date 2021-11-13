47d87d23e89671b4150f17a1f1f29a59988c3f76
Ivan Cardoso <cardoso.ivan2012@gmail.com>
2021-11-13 11:15:31 -0300

    Implementação do hashCode e equals das classes Pessoa e Conta

--- code/Strategy/Conta.java
+++ code/Strategy/Conta.java
@@ -47,4 +47,26 @@ public class Conta {
 	public double getValorChequeEspecial() {
 		return valorChequeEspecial;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + numeroConta;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Conta other = (Conta) obj;
+		if (numeroConta != other.numeroConta)
+			return false;
+		return true;
+	}
 }
