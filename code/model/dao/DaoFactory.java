c30eaaae5d301d6685db87b447ee6513cde28a55
Ivan Cardoso <cardoso.ivan2012@gmail.com>
2021-11-23 20:44:52 -0300

    Adição de mais métodos na fábrica de conexões DaoFactory

--- code/model/dao/DaoFactory.java
+++ code/model/dao/DaoFactory.java
@@ -2,8 +2,12 @@ package model.dao;
 
 import dbConnection.DBConnection;
 import model.dao.implementations.ContaDaoJDBC;
+import model.dao.implementations.DepositoDaoJDBC;
 import model.dao.implementations.PessoaFisicaDaoJDBC;
 import model.dao.implementations.PessoaJuridicaDaoJDBC;
+import model.dao.implementations.SaqueDaoJDBC;
+import model.dao.implementations.TaxaDaoJDBC;
+import model.dao.implementations.TransferenciaDaoJDBC;
 
 public class DaoFactory {
 
@@ -17,5 +21,21 @@ public class DaoFactory {
 	
 	public static ContaDao createContaDao() {
 		return new ContaDaoJDBC(DBConnection.getConnection());
+	}
+	
+	public static DepositoDao createDepositoDao() {
+		return new DepositoDaoJDBC(DBConnection.getConnection());
+	}
+	
+	public static SaqueDao createSaqueDao() {
+		return new SaqueDaoJDBC(DBConnection.getConnection());
+	}
+	
+	public static TaxaDao createTaxaDao() {
+		return new TaxaDaoJDBC(DBConnection.getConnection());
+	}
+	
+	public static TransferenciaDao createTransferenciaDao() {
+		return new TransferenciaDaoJDBC(DBConnection.getConnection());
 	}
 }
